0ead457894ab83b2c0ed5172db1b2c6d2e693aa6
M.B <mb0583219718@gmail.com>
2020-05-20 10:18:14 +0300

    this is the server for corona js

---
+++ example/example/Models/Locations.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace example.Models
+{
+    public class Locations
+    {
+        public string PatientID { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public string Location { get; set; }
+
+        public string City { get; set; }
+
+        public Locations()
+        {
+
+        }
+        
+        public Locations(string patientID,DateTime startDate, DateTime endDate,string location,string city)
+        {
+            this.PatientID = patientID;
+            this.StartDate = startDate;
+            this.EndDate = endDate;
+            this.Location = location;
+            this.City = city;
+        }
+    }
+
+    
+
+}
